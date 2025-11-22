54015fc76aa7c4efdd370264faf2f0573046dda6
dennysanthosh12 <125438235+dennysanthosh12@users.noreply.github.com>
2025-11-22 21:06:58 +0530

    updated customer service

--- CustomerService/src/main/java/com/bank/CustomerService/Controller/CustomerController.java
+++ CustomerService/src/main/java/com/bank/CustomerService/Controller/CustomerController.java
@@ -1,6 +1,16 @@
 package com.bank.CustomerService.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("Customer")
 public class CustomerController {
 
+	@GetMapping("/hello")
+	public String gethello() {
+		return "Hello from Denny";
+	}
+	
 }
