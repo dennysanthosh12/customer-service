3312ce11f139afc72e212b1a0bfefa64d34c405e
dennysanthosh12 <125438235+dennysanthosh12@users.noreply.github.com>
2025-12-11 14:06:14 +0530

    Update CustomerController.java

--- CustomerService/src/main/java/com/bank/CustomerService/Controller/CustomerController.java
+++ CustomerService/src/main/java/com/bank/CustomerService/Controller/CustomerController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bank.CustomerService.DTO.CustomerDTO;
-import com.bank.CustomerService.Model.Customer;
 import com.bank.CustomerService.Service.CustomerService;
 
 @RestController
